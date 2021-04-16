0626b1b0ab6d12ebf0ddbe570f23b430ace0db2d
such-an-inbetweener <alexenderjuror@gmail.com>
2021-04-16 21:28:21 +0300

    init 3

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Ohhh shit here we go again ("/>
+    <HelloWorld msg="fdjgdskj"/>
   </div>
 </template>
 

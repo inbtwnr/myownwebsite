f77bcdb9421651bc31545564aba6c93941983648
such-an-inbetweener <alexenderjuror@gmail.com>
2021-04-20 10:46:49 +0300

    Skills list

--- src/components/SkillsList.vue
+++ src/components/SkillsList.vue
@@ -1,9 +1,11 @@
 <template>
-    <ul id="array-1">
-        <li v-for="(category) in categories" :key = "category.id">
-            {{ category.skillCategory }}: {{ category.skills }}
-        </li>
-    </ul>
+    <div class="paragraph1">
+        <ul class="listWrapper" id="array-1">
+            <li v-for="(category) in categories" :key = "category.id" class="listContainer">
+                <h4>{{ category.skillCategory }}: </h4> <p class="paragraph2">{{ category.skills }}</p> 
+            </li>
+        </ul>
+    </div>
 </template>
 <script>
 export default {

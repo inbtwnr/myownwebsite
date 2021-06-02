43c7ca3fb4c4715aabab9b2640a81eb0763eaaf2
Sasha <alexanderjuror@gmail.com>
2021-06-02 16:28:09 +0300

    Revert "prebuild fixes"
    This reverts commit c16bfddf68d3f7dfb8597d0da6a3ef223952d43d.

--- server.js
+++ server.js
@@ -2,12 +2,8 @@ const express = require("express");
 const serveStatic = require("serve-static");
 const path = require("path");
 
-const app = express();
-
+const app = express(); 
 app.use("/", serveStatic(path.join(__dirname, "/dist")));
-
-const port = process.env.PORT || 8080;
+const port = process.env.port || 8080;
 app.listen(port);
-
-
 console.log("Server is running at" + port);
